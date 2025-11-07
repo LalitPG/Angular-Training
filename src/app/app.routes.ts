09b5051397c612f490a59c036515cf4d4a83cc18
lalit.patil <lalit.patil@nihilent.com>
2025-11-07 09:48:23 +0530

    addtocart changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,18 @@ import { UpdateComponent } from './catalog/products/update/update.component';
 import { SignInComponent } from './membership/sign-in/sign-in.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './membership/register/register.component';
+import { DeleteComponent } from './catalog/products/delete/delete.component';
+import { CartComponent } from './shopping-cart/cart/cart.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'lists', component: ListComponent },
   { path: 'details/:id', component: DetailsComponent },
   { path: 'update/:id', component: UpdateComponent },
+  { path: 'delete/:id', component: DeleteComponent },
   { path: 'login', component:  SignInComponent},
   { path: 'home', component:  HomeComponent},
-  { path: 'register', component:  RegisterComponent}
+  { path: 'addtocart', component:  CartComponent},
+  { path: 'register', component:  RegisterComponent},
+  { path: 'cart', component:  CartComponent}
 ];
--- src/app/catalog/products/details/details.component.ts
+++ src/app/catalog/products/details/details.component.ts
@@ -5,6 +5,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from '../../product.service';
 import { Product } from '../../models/product';
 import { CounterComponent } from '../counter/counter.component';
+import { Item as CartItem }from '../../../shopping-cart/models/Item'
 @Component({
   selector: 'app-details',
   standalone: true,
@@ -14,8 +15,11 @@ import { CounterComponent } from '../counter/counter.component';
 })
 export class DetailsComponent implements OnInit {
 
-currentProductId:any;
+  currentProductId:any;
   @Input () product: Product|undefined;
+  private storageKey = 'shoppingCart';
+  cartItems: CartItem[] = [];
+  cartItem! : CartItem;
 
   constructor(private router:Router,private route: ActivatedRoute, 
               private productService: ProductService) {  }
@@ -27,14 +31,20 @@ currentProductId:any;
  
   onUpdate(data:any){
      if(this.product != undefined)
-        this.product.likes=data.count;
+      this.product.likes=data.count;
       this.productService.updateProduct(this.product);
    }
    
   addToCart():void {
+    
     let id=this.currentProductId;
     console.log("Adding to cart product with id:", id);
-    this.router.navigate(['./addtocart/', id]);
+    this.product = this.productService.getProductById(this.currentProductId);
+    this.cartItems = JSON.parse(sessionStorage.getItem(this.storageKey) || '[]');
+    this.cartItem = new CartItem(this?.product!.id,this.product!.title,this.product!.price,1,this.product!.imageurl);
+    this.cartItems.push(this.cartItem);
+    sessionStorage.setItem(this.storageKey, JSON.stringify(this.cartItems));
+    this.router.navigate(['./addtocart']);
   }
 
   goToUpdate(): void {
@@ -47,20 +57,6 @@ currentProductId:any;
     console.log("Deleting product with id:", id);
     this.router.navigate(['./delete/', id]);
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
    // ngIf
   isLoggedIn = true;
   userName = 'Ravi';
